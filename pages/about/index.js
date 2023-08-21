820c7cebf3b0c8e68094e660c3673c45107d7911
Ruben Garcia <ruben.garcia.ext@idrica.com>
2023-08-21 12:11:50 +0200

    Services addons

--- pages/about/index.js
+++ pages/about/index.js
@@ -97,6 +97,9 @@ import Circles from '../../components/Circles';
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
+// counter
+import CountUp from "react-countup";
+
 const About = () => {
   const [index, setIndex] = useState(0);
   return (
@@ -113,7 +116,34 @@ const About = () => {
         <Avatar />
       </motion.div>
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-        <div className='flex-1 flex flex-col justify-center'>text</div>
+        <div className='flex-1 flex flex-col justify-center'>
+          <h2 className='h2'>Captivating <span className='text-accent'>stories</span> birth magnificent designs.</h2>
+          <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>10 years ago, I began freelancing as a developer. Since then, I've done remote
+            work for agencies, consulted for startups, and collaborated on digital products
+            for business and consumer use.
+          </p>
+          {/* counters */}
+          <div>
+            <div>
+              {/* experience */}
+              
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute
+              after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={4} duration={5} /> +
+                </div>
+              </div>
+              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-'>Years of experience</div>
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute
+              after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={250} duration={5} /> +
+                </div>
+              </div>
+              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Satisfied clients</div>
+            </div>
+          </div>
+        </div>
         <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
             {aboutData.map((item, itemIndex) => {
